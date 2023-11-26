60ed1f6f3ae83449717ddf9171801c2eaf8acec3
aautin <ezagof@gmail.com>
2023-11-26 16:27:04 +0100

    bonus_part done and main part too, normed

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 12:44:07 by aautin            #+#    #+#             */
-/*   Updated: 2023/11/26 16:21:45 by aautin           ###   ########.fr       */
+/*   Updated: 2023/11/26 16:13:44 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static void	ft_get_afterline(char *str, char *substr)
 {
@@ -68,52 +68,55 @@ static size_t	ft_read_and_protect(int fd, char *buffer_str)
 	return (readchars);
 }
 
-static char	*ft_notbuffsizeread(int sz, char *buffer_str, char *line)
+static char	*ft_notbuffsizeread(int sz, int fd, char **buffer_str, char *line)
 {
 	if (sz == -1)
-	{
-		free(line);
-		return (NULL);
-	}
+		return (ft_freenullreturn(buffer_str[fd], line));
 	if (ft_strchr(line, '\n'))
 	{
-		ft_get_afterline(line, buffer_str);
+		ft_get_afterline(line, buffer_str[fd]);
 		return (ft_get_beforeline(line, 1));
 	}
 	else
 	{
-		if (line[0])
-		{
-			buffer_str[0] = '\0';
-			return (line);
-		}
-		else
+		if (!line[0] && !buffer_str[fd][0])
 		{
 			free(line);
 			return (NULL);
 		}
+		if (line[0])
+		{
+			buffer_str[fd][0] = '\0';
+			return (line);
+		}
+		else
+			return (ft_freenullreturn(buffer_str[fd], line));
 	}
 }
 
 char	*get_next_line(int fd)
 {
-	static char	buffer_str[BUFFER_SIZE + 1];
+	static char	*buffer_str[1024];
 	char		*line;
 	long int	readchars;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	if (buffer_str[0] != '\0')
-		line = ft_strjoin(buffer_str, "", 0);
+	if (buffer_str[fd])
+		line = ft_strjoin(buffer_str[fd], "", 0);
 	else
+	{
+		buffer_str[fd] = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		buffer_str[fd][0] = '\0';
 		line = ft_strjoin("", "", 0);
+	}
 	while (!ft_strchr(line, '\n'))
 	{
-		readchars = ft_read_and_protect(fd, buffer_str);
-		line = ft_strjoin(line, buffer_str, 1);
+		readchars = ft_read_and_protect(fd, buffer_str[fd]);
+		line = ft_strjoin(line, buffer_str[fd], 1);
 		if (readchars != BUFFER_SIZE)
-			return (ft_notbuffsizeread(readchars, buffer_str, line));
+			return (ft_notbuffsizeread(readchars, fd, buffer_str, line));
 	}
-	ft_get_afterline(line, buffer_str);
+	ft_get_afterline(line, buffer_str[fd]);
 	return (ft_get_beforeline(line, 1));
 }
