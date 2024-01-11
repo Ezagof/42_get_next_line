3d8b823ef98aea9cd1efdda72dfe9c00ac3b35a9
aautin <ezagof@gmail.com>
2024-01-11 17:23:51 +0100

    reimplemented completely gnl and gnl_bonus, everythg on the static memarea, just 5 functions

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -5,27 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/23 12:45:04 by aautin            #+#    #+#             */
-/*   Updated: 2023/11/28 21:00:40 by aautin           ###   ########.fr       */
+/*   Created: 2024/01/09 16:37:08 by aautin            #+#    #+#             */
+/*   Updated: 2024/01/11 17:19:12 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include <fcntl.h>
+# include <stddef.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 80
+#  define BUFFER_SIZE 142
 # endif
 
-void	ft_free(void *mem);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-char	*ft_freenullreturn(char *s1, char *s2);
-char	*ft_strjoin(char *s1, char *s2, char free);
+// get_next_line_utils_bonus.c
+char	*string_chr(char *str, char letter);
+
+// get_next_line_bonus.c
+char	*string_join(char *s1, char *s2, char flag);
+void	set_string_after_newline(char *buffer);
+char	*turn_string_into_firstline(char *temp);
 char	*get_next_line(int fd);
 
 #endif
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -5,71 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/23 12:44:11 by aautin            #+#    #+#             */
-/*   Updated: 2023/11/26 16:22:37 by aautin           ###   ########.fr       */
+/*   Created: 2024/01/09 18:56:47 by aautin            #+#    #+#             */
+/*   Updated: 2024/01/11 17:18:12 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-size_t	ft_strlen(const char *s)
+char	*string_chr(char *str, char letter)
 {
-	size_t	i;
+	int	i;
 
-	if (!s)
-		return (0);
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	int		i;
-
-	i = 0;
-	if (!s)
-		return (NULL);
-	while (s[i])
-	{
-		if (s[i] == (char) c)
-			return ((char *) &s[i]);
-		i++;
-	}
-	if ((char) c == '\0')
-		return ((char *) &s[i]);
-	return (NULL);
-}
-
-char	*ft_freenullreturn(char *s1, char *s2)
-{
-	free(s1);
-	free(s2);
-	return (NULL);
-}
-
-char	*ft_strjoin(char *s1, char *s2, char freed)
-{
-	char	*str;
-	int		i;
-	int		j;
-
-	if (!s1 || !s2)
-		return (NULL);
-	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (!str)
-		return (NULL);
 	i = -1;
-	while (s1[++i])
-		str[i] = s1[i];
-	j = -1;
-	while (s2[++j])
-		str[i + j] = s2[j];
-	str[i + j] = '\0';
-	if ((freed == 1 || freed == 3) && s1)
-		free(s1);
-	if ((freed == 2 || freed == 3) && s2)
-		free(s2);
-	return (str);
+	while (str[++i])
+	{
+		if (str[i] == letter)
+			return (&str[i]);
+	}
+	return (NULL);
 }
